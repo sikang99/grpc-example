665bb7001568f3da5ad63f1a9e86adcfbf1e90be
Stoney Kang <sikang99@gmail.com>
2015-05-01 21:36:25 +0900

    sorted by key = id

--- server/main.go
+++ server/main.go
@@ -28,6 +28,7 @@ func NewCustomerService() *customerService {
 
 func (cs *customerService) ListPersons(p *pb.RequestType, stream pb.CustomerService_ListPersonsServer) error {
 	log.Printf("list (%v)\n", p.Man)
+
 	cs.Lock()
 	defer cs.Unlock()
 
@@ -75,10 +76,10 @@ func (cs *customerService) ListPersons(p *pb.RequestType, stream pb.CustomerServ
 
 func (cs *customerService) GetPerson(c context.Context, p *pb.Person) (*pb.ResponseType, error) {
 	log.Printf("get (%d)\n", p.Id)
+	resp := new(pb.ResponseType)
+
 	cs.Lock()
 	defer cs.Unlock()
-
-	resp := new(pb.ResponseType)
 
 	ps, ok := cs.customers[int(p.Id)]
 	if ok {
@@ -91,14 +92,14 @@ func (cs *customerService) GetPerson(c context.Context, p *pb.Person) (*pb.Respo
 
 func (cs *customerService) AddPerson(c context.Context, p *pb.Person) (*pb.ResponseType, error) {
 	log.Printf("add (%d)\n", cs.id)
+	resp := new(pb.ResponseType)
+
 	cs.Lock()
 	defer cs.Unlock()
 
 	p.Id = int32(cs.id)
 	cs.customers[int(p.Id)] = p
 	cs.id++
-
-	resp := new(pb.ResponseType)
 
 	ps, ok := cs.customers[int(p.Id)]
 	if ok {
@@ -111,10 +112,10 @@ func (cs *customerService) AddPerson(c context.Context, p *pb.Person) (*pb.Respo
 
 func (cs *customerService) DeletePerson(c context.Context, p *pb.Person) (*pb.ResponseType, error) {
 	log.Printf("delete (%d)\n", p.Id)
+	resp := new(pb.ResponseType)
+
 	cs.Lock()
 	defer cs.Unlock()
-
-	resp := new(pb.ResponseType)
 
 	ps, ok := cs.customers[int(p.Id)]
 	if ok {
@@ -128,10 +129,10 @@ func (cs *customerService) DeletePerson(c context.Context, p *pb.Person) (*pb.Re
 
 func (cs *customerService) UpdatePerson(c context.Context, p *pb.Person) (*pb.ResponseType, error) {
 	log.Printf("update (%d)\n", p.Id)
+	resp := new(pb.ResponseType)
+
 	cs.Lock()
 	defer cs.Unlock()
-
-	resp := new(pb.ResponseType)
 
 	_, ok := cs.customers[int(p.Id)]
 	if ok {
